3fbeb0c2452d2c46a0cd342e2e1970956c2e5342
fizzzzzzzz <294109348@qq.com>
2018-08-21 21:29:59 +0800

    Update how_to_do.py

--- how_to_do.py
+++ how_to_do.py
@@ -1,6 +1,11 @@
 def is_palindrome(n):        # 筛选出回数， 121 1234321 等
     s = str(n)
     return s == s[::-1]
+
+def by_score(t):             #元组排序方法
+    return -t[1]
     
 if __name__ == '__main__':
     print(list(filter(is_palindrome, range(1000))))
+    L = [('Bob', 75), ('Adam', 92), ('Bart', 66), ('Lisa', 88)]
+    L_score = sorted(L, key=by_score)
